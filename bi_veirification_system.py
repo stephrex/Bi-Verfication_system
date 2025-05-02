6ddcad9ff3a44fc21dc09f1ba50c43d209ec7ff6
Oloyede Iremide <115434024+stephrex@users.noreply.github.com>
2025-05-02 12:33:03 +0100

    Update bi_veirification_system.py

--- bi_veirification_system.py
+++ bi_veirification_system.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Test File Upload")
 
-file = st.file_uploader("Upload image", type=["jpg", "jpeg", "png", "BMP"])
+file = st.file_uploader("Upload image")
 if file:
     st.image(file)
